bd61a2745d11c201e4bbb78f1ea5af76bad41930
dxm <meetdxm@163.com>
2026-05-31 20:47:49 +0800

    更新打包路径

--- 打包.bat
+++ 打包.bat
@@ -10,7 +10,7 @@ echo [1/3] 检查并安装依赖...
 pip install flask pywebview pyinstaller pypinyin natsort -q
 
 echo [2/3] 开始打包（可能需要几分钟）...
-pyinstaller --onefile --windowed --add-data "templates;templates" --add-data "icon.ico;." --icon="icon.ico" --name "file-warehouse" app.py
+pyinstaller --onefile --windowed --distpath "." --add-data "templates;templates" --add-data "icon.ico;." --icon="icon.ico" --name "file-warehouse" app.py
 
 echo.
 echo [3/3] 打包完成！
